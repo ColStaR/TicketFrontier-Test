e07c9ec724658e1baae2b095f321c5a8c2f4e499
ColStaR <starshootr@gmail.com>
2017-08-30 18:25:36 -0700

    Setting Foundation for Test 1
    Created TEST1 Project in Netbeans.
    Created print test, which prints properly.
    Added Jsoup 1.10.3 library to project.

---
+++ TEST2/src/test2/TEST2.java
@@ -0,0 +1,18 @@
+package test2;
+
+/**
+ * Ticket Frontier Technical Test, Test 2.
+ * @author Ryan Wong
+ */
+public class TEST2
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Test");
+    }
+    
+}
