873ca3eb061fec9824fb77ddf2a44923313bb17f
ColStaR <starshootr@gmail.com>
2017-08-30 19:19:28 -0700

    Added JSoup functionality, Titles and Links
    Added the ability to identify table titles and destination URLs for each table given.

--- TEST2/src/test2/TEST2.java
+++ TEST2/src/test2/TEST2.java
@@ -1,7 +1,14 @@
 package test2;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 /**
  * Ticket Frontier Technical Test, Test 2.
+ *
  * @author Ryan Wong
  */
 public class TEST2
@@ -10,9 +17,31 @@ public class TEST2
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
-        System.out.println("Test");
+        String url = "http://www2.stat.duke.edu/courses/Spring01/sta114/data/andrews.html";
+        String urlBegin = "http://www2.stat.duke.edu/courses/Spring01/sta114/data/";
+        Document doc = Jsoup.connect(url).get();
+//        Element rawText = doc.body();
+//        String line = rawText.toString();
+//        System.out.println(line);        
+
+        Elements content = doc.select("tr");
+
+        for (Element a : content)
+        {
+//            System.out.println(a);
+            String title = a.select("td").text();
+            String link = a.select("a").attr("href");
+            System.out.println("Title: " + title.toString());
+
+            // Links only appear as relative within JSoup.. This combines the beginning links and the relative links to make a full URL.
+            String urlFull = urlBegin + link;
+            System.out.println("Link: " + urlFull);
+            System.out.println("#####");
+
+        }
+
     }
-    
+
 }
