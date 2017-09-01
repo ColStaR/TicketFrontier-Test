cbf3b5a33aef1e9afc03a4a360807cf195dc0e3b
ColStaR <starshootr@gmail.com>
2017-08-31 19:24:28 -0700

    Implemented disasterRecord parsing, saving
    Added a parseLine function which cleans up a line of data, gathers the necessary information, stores it into a disasterRecord object, and then stores that disasterRecord into the recordsList list for easier traversal.

--- TEST3/src/test3/TEST3.java
+++ TEST3/src/test3/TEST3.java
@@ -5,11 +5,12 @@
  */
 package test3;
 
-
 import java.io.*;
+import java.util.*;
 
 /**
  * TicketFrontier Test 3
+ *
  * @author Ryan
  */
 public class TEST3
@@ -21,26 +22,80 @@ public class TEST3
     public static void main(String[] args)
     {
         String fileName = args[0];
+        ArrayList<disasterRecord> recordsList = new ArrayList<disasterRecord>();
 
         // This file reading code comes from an earlier Java project for
         // a class at the SRJC, CS 17.11.It has been modified slightly to
         // accept the filename scheme.
-        try {
+        try
+        {
             String line = "";
-            FileReader fileReader =  new FileReader(fileName);
+            FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            while((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
-            }   
-            bufferedReader.close();         
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                parseLine(recordsList, line);
+//                System.out.println("#####");
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException ex)
+        {
+            System.out.println("Unable to open file '" + fileName + "'");
+        } catch (IOException ex)
+        {
+            System.out.println("Error reading file '" + fileName + "'");
         }
-        catch(FileNotFoundException ex) {
-            System.out.println("Unable to open file '" + fileName + "'");                
-        }
-        catch(IOException ex) {
-            System.out.println("Error reading file '"  + fileName + "'");        
+        
+        for(disasterRecord record: recordsList){
+            System.out.println(record.getRecordNum());
         }
     }
-    
+
+    public static void parseLine(ArrayList<disasterRecord> record, String line)
+    {
+        // The current lines being parsed from the table are filled with spaces,
+        // which will complicate a simple string split due to differing numbers
+        // of spaces as record numbers increase. As such, we will clean the
+        // date by removing all excess spaces.
+        // The regex help comes from a Stack Overflow example here:
+        // https://stackoverflow.com/questions/2932392/java-how-to-replace-2-or-more-spaces-with-single-space-in-string-and-delete-lead
+        String shortLine = line.replaceAll(" +", " ");
+
+        // Through rigorous research, I found that splitting between spaces
+        // divides the String into 18 substrings. Since this number does not
+        // change in size, it is set to an array for performance.
+        String dividedLine[] = new String[21];
+//        System.out.println(shortLine);
+        dividedLine = shortLine.split(" ");
+        // Useful Indexes:
+        // 1 = table number
+        // 2 = sub-table number
+        // 3 = record number
+        // 4 = day
+        // 5 = day of month
+        // 6 = month
+        // 7 = year
+        // 8 = Running total of deaths that year
+        // 10 = Wounded in that accident
+//                for(int a=0; a < 10; a++){
+//                    System.out.print(dividedLine[a] + "|");
+//                }
+
+        int tableNum = Integer.parseInt(dividedLine[1]); 
+        int tableSubNum = Integer.parseInt(dividedLine[2]);
+        int recordNum = Integer.parseInt(dividedLine[3]);
+        String dateDay = dividedLine[4];
+        int dateNum = Integer.parseInt(dividedLine[5]);
+        String dateMonth = dividedLine[6];
+        int dateYear = Integer.parseInt(dividedLine[7]);
+        int totalDeaths = Integer.parseInt(dividedLine[8]);
+        int recordWounded = Integer.parseInt(dividedLine[10]);
+
+        // Construct a new disasterRecord, and add it to the program's list.
+        disasterRecord newRecord = new disasterRecord(tableNum, tableSubNum, 
+        recordNum, dateDay, dateNum, dateMonth, dateYear, totalDeaths, 
+        recordWounded);
+        record.add(newRecord);
+    }
 }
